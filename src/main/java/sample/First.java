d7bab7cd14b0e956b0a83f4f68ba58bb39adc3df
Abdulrazzaq1234 <razzaqallan2000@gmail.com>
2023-06-08 20:42:45 +0530

    1st commit

---
+++ src/main/java/sample/First.java
@@ -0,0 +1,28 @@
+package sample;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+;
+
+public  class First extends GenericServlet
+{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("monster");
+		res.getWriter().print("monster");	
+	}
+
+	
+	
+
+
+	
+
+}
---
+++ src/main/java/sample/Second.java
@@ -0,0 +1,21 @@
+package sample;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Second extends GenericServlet
+{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+	    System.out.println("welcome to second");
+		res.getWriter().println("welcome to second");
+	
+		
+	}
+
+}
